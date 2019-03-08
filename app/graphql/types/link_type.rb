acc97c9a8470f874a108159abbcbd4a0cb2039a4
Romain Vigo Benia <romain.vigobenia@gmail.com>
2019-03-08 15:36:52 +0100

    remove Link model

--- app/graphql/types/link_type.rb
+++
@@ -1,7 +0,0 @@
-module Types
-  class LinkType < BaseObject
-    field :id, ID, null: false
-    field :url, String, null: false
-    field :description, String, null: false
-  end
-end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,17 +1,12 @@
 module Types
   class QueryType < Types::BaseObject
     # queries are just represented as fields
-    # `all_links` is automatically camelcased to `allLinks`
-    field :all_links, [LinkType], null: false
+    # `all_objectives` is automatically camelcased to `allObjectives`
     field :all_objectives, [ObjectiveType], null: false
     field :all_steps, [StepType], null: false
     field :all_tasks, [TaskType], null: false
 
-    # this method is invoked, when `all_link` fields is beeing resolved
-    def all_links
-      Link.all
-    end
-
+    # this method is invoked, when `all_objectives` fields is beeing resolved
     def all_objectives
       Objective.all
     end
